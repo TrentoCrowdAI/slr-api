c68395a45b11546fe5882fc2d5202bfdc106f79d
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2019-03-08 18:51:13 +0100

    add very basic papers controller and its basic tests

--- src/controllers/papers.controller.js
+++ src/controllers/papers.controller.js
@@ -2,13 +2,15 @@
 // as services using express
 
 const express = require('express');
-//const papersDelegate = require(__base + 'delegates/papers.delegate');
+const papersDelegate = require(__base + 'delegates/papers.delegate');
 
 const router = express.Router();
 
 router.get('/search', async (req, res, next) => {
   try {
-    res.status(201).json({"msg": "search"});
+    let query = req.query.query;
+    let papers = await papersDelegate.selectBySingleKeyword(query,3,0,"id","ASC");
+    res.status(200).json(papers);
   } catch (e) {
     // we delegate to the error-handling middleware
     next(e);
@@ -17,8 +19,14 @@ router.get('/search', async (req, res, next) => {
 
 router.get('/papers/:id', async (req, res, next) => {
   try {
-    let paper_id = req.params.id;
-    res.status(201).json({"id": paper_id});
+    let paper_id = parseInt(req.params.id, 10);
+    if (isNaN(paper_id)) {
+      res.status(403).json({"statusCode" : 403, "message" : "the id is not a number"});
+      return;
+    }
+    let paper = await papersDelegate.selectById(paper_id);
+    paper.content = JSON.parse(paper.content);
+    res.status(200).json(paper);
   } catch (e) {
     // we delegate to the error-handling middleware
     next(e);
--- src/controllers/papers.controller.spec.js
+++ src/controllers/papers.controller.spec.js
@@ -6,6 +6,19 @@ test('dummy test', () => {
   expect(true).toBe(true);
 })
 
+test('GET /search should return 400 if mandatory field is not present', async () => {
+  jest.setTimeout(10000);
+  let response = await request(app).get('/search');
+  expect(response.status).toBe(400)
+});
+
+test('GET /papers/17 should return 200 and paper', async () => {
+  jest.setTimeout(10000);
+  let response = await request(app).get('/papers/17');
+  expect(response.status).toBe(200)
+  expect(response.body.id).toBeDefined();
+});
+
 // test('POST /jobs should return 400 if mandatory fields are not present', async () => {
 //   let response = await request(app).post('/jobs');
 //   expect(response.status).toBe(400);
