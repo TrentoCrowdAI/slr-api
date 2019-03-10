0d1c6b98a2fd6d8923eb8f87faa4a47b656c1384
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2019-03-10 13:02:25 +0100

    tentative fix openhandles travis

--- src/app.spec.js
+++ src/app.spec.js
@@ -27,7 +27,3 @@ test('GET / should return 200 (using supertest)', async () => {
   expect(response.statusCode).toBe(200);
 });
 
-// open handle fix -> https://github.com/visionmedia/supertest/issues/520#issuecomment-469044925
-afterAll(async () => {
-	await new Promise(resolve => setTimeout(() => resolve(), 500)); // avoid jest open handle error
-});
