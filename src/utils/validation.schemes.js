3533f692796c014d8f3e84832beb5e8443bcf0b3
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2019-06-29 16:43:51 +0200

    csv paper fields validation scheme allows empty string

--- src/utils/validation.schemes.js
+++ src/utils/validation.schemes.js
@@ -70,8 +70,64 @@ const paper = {
 //is the same with paper object
 const projectPaper = paper;
 
-//is the same with projectPaper
-const csvPaperFields = projectPaper;
+const csvPaperFields = {
+    "type": "object",
+    "properties": {
+        "authors": {
+            "type": "string"
+        },
+        "title": {
+            "type": "string"
+        },
+        "year": {
+            "type": "string"
+        },
+        "date": { /* temporarly unused */
+            "type": "string"
+        },
+        "source_title": {
+            "type": "string"
+        },
+        "link": {
+            "type": "string"
+        },
+        "abstract": {
+            "type": "string"
+        },
+        "document_type": {
+            "type": "string"
+        },
+        "source": {
+            "type": "string"
+        },
+        "eid": {
+            "type": "string"
+        },
+        "abstract_structured": {
+            "type": "string",
+
+        },
+        "filter_oa_include": {
+            "type": "string",
+
+        },
+        "filter_study_include": {
+            "type": "string",
+
+        },
+        "notes": {
+            "type": "string",
+
+        },
+        "manual": {
+            "type": "string"
+        },
+        "doi": {
+            "type": "string"
+        }
+    },
+    "required": ["authors", "title", "year", "source_title", "link", "abstract", "document_type", "source", "abstract_structured", "filter_oa_include", "filter_study_include", "notes", "manual", "doi"]
+};
 
 //scheme of project, has 2 attributes
 const project = {
